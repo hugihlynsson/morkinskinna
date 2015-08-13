12cb22bafe85470ba090d66d74f010f633f7972c
Hugi Hlynsson <hugihlynsson@gmail.com>
2015-08-13 22:11:51 +0000

    Add november and oktober parallax forms

--- js/main.js
+++ js/main.js
@@ -6,22 +6,64 @@ $('.event__header').on('click', function(event) {
 });
 
 var scroller = {
+
   initialize: function() {
+    this.updateOffsets();
+    window.addEventListener('resize', this.updateOffsets.bind(this), false);
     window.addEventListener('scroll', this.onScroll.bind(this), false);
   },
+
+  items: [
+    {
+      element: $('.form__part--august'),
+      speedDivider: 1,
+    },
+    {
+      element: $('.form__part--september'),
+      speedDivider: 2,
+    },
+    {
+      element: $('.form__part--oktober'),
+      speedDivider: 1,
+    },
+    {
+      element: $('.form__part--november1'),
+      speedDivider: 2,
+    },
+    {
+      element: $('.form__part--november2'),
+      speedDivider: 2.1,
+    },
+  ],
+
+  updateOffsets: function() {
+    this.windowHeight = $(window).height();
+    this.items = this.items.map(function(item) {
+      item.svg = item.element.find('.form__part__svg');
+      item.offset = item.element.offset().top;
+      return item;
+    });
+    this.onScroll();
+  },
+
   lastScrollY: 0,
+
   ticking: false,
+
   onScroll: function() {
-    this.lastScrollY = window.pageYOffset;
     if (!this.ticking) {
+      this.lastScrollY = window.pageYOffset;
       window.requestAnimationFrame(this.update.bind(this));
     }
   },
+
   update: function() {
-    this.translateHelper($('.form__part--august'), -this.lastScrollY/2);
-    this.translateHelper($('.form__part--september'), -this.lastScrollY/3);
+    this.items.forEach(function(item) {
+      this.translateHelper(item.svg, -(this.lastScrollY - item.offset + this.windowHeight*1.5)/item.speedDivider);
+    }.bind(this));
     this.ticking = false;
   },
+
   translateHelper: function($element, amount) {
     var translationStyle = 'translate3d(0px,' + amount + 'px, 0px)';
     $element.css({
