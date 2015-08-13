bb0d285e5f291848ed2cf663406aac6e82621ca5
Hugi Hlynsson <hugihlynsson@gmail.com>
2015-08-13 14:51:04 +0000

    Add form parallax demo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
+"use strict";
+
 $('.event__header').on('click', function(event) {
   event.preventDefault();
   $(this).siblings('.event__details').slideToggle();
 });
 
-console.log('loaded scripts');
+var scroller = {
+  initialize: function() {
+    window.addEventListener('scroll', this.onScroll.bind(this), false);
+  },
+  lastScrollY: 0,
+  ticking: false,
+  onScroll: function() {
+    this.lastScrollY = window.pageYOffset;
+    if (!this.ticking) {
+      window.requestAnimationFrame(this.update.bind(this));
+    }
+  },
+  update: function() {
+    this.translateHelper($('.form__part--august'), -this.lastScrollY/2);
+    this.translateHelper($('.form__part--september'), -this.lastScrollY/3);
+    this.ticking = false;
+  },
+  translateHelper: function($element, amount) {
+    var translationStyle = 'translate3d(0px,' + amount + 'px, 0px)';
+    $element.css({
+      '-webkit-transform': translationStyle,
+      '-moz-transform': translationStyle,
+      '-ms-transform': translationStyle,
+      '-o-transform': translationStyle,
+      'transform': translationStyle,
+    })
+  },
+};
+
+scroller.initialize();
+
+
+console.log('script loaded');
